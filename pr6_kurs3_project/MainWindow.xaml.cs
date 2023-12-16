48e2a5bb1c5aa6fe25b146264b9f4cb7bc4f137c
vas-web <nikarah123000@gmail.com>
2023-12-16 07:57:34 +0300

    ага

--- pr6_kurs3_project/MainWindow.xaml.cs
+++ pr6_kurs3_project/MainWindow.xaml.cs
@@ -24,6 +24,42 @@ namespace pr6_kurs3_project
 		{
 			InitializeComponent();
 		}
+		private void Button_Click_2(object sender, RoutedEventArgs e)
+		{
+			SetCalc.SetParams(1);
+			tb_a2.Text = Convert.ToString(SetCalc._a);
+			tb_b2.Text = Convert.ToString(SetCalc._b);
+			tb_c2.Text = Convert.ToString(SetCalc._c);
+		}
 
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			SetCalc.SetParams(10);
+			tb_a2.Text = Convert.ToString(SetCalc._a);
+			tb_b2.Text = Convert.ToString(SetCalc._b);
+			tb_c2.Text = Convert.ToString(SetCalc._c);
+		}
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			int a = Convert.ToInt32(tb_a.Text);
+			int b = Convert.ToInt32(tb_b.Text);
+			int c = Convert.ToInt32(tb_c.Text);
+			SetCalc.SetParams(a, b, c);
+		}
+
+		private void Button_Click_3(object sender, RoutedEventArgs e)
+		{
+			SetCalc.SetIncrem();
+			tb_incr.Text = Convert.ToString(SetCalc._a + " ; " + SetCalc._b + " ; " + SetCalc._c);
+		}
+
+		private void Button_Click_4(object sender, RoutedEventArgs e)
+		{
+			int summa = SetCalc.TriadSumPlusValue(0);
+			int valuehz = Convert.ToInt32(tb_sum.Text);
+			int sumplus = SetCalc.TriadSumPlusValue(summa, valuehz, 0);
+			tb_sumplus.Text = Convert.ToString(summa + " ; " + sumplus);
+
+		}
 	}
 }
---
+++ pr6_kurs3_project/SetCalc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr6_kurs3_project
+{
+	internal class SetCalc
+
+	{
+		public static int _a { get; set; }
+		public static int _b { get; set; }
+		public static int _c { get; set; }
+		public static void SetParams(int init_a, int init_b, int init_c)
+		{
+			_a = init_a;
+			_b = init_b;
+			_c = init_c;
+		}
+		public static void SetParams(int value)
+		{
+			_a += value;
+			_b += value;
+			_c += value;
+
+		}
+		public static void SetIncrem()
+		{
+			_a++;
+			_b++;
+			_c++;
+		}
+		public static int TriadSumPlusValue(int sum)
+		{
+			sum = _a + _a + _c;
+			return sum;
+		}
+		public static int TriadSumPlusValue(int sum, int value, int newsum)
+		{
+			newsum = sum + value;
+			return newsum;
+		}
+	}
+}
